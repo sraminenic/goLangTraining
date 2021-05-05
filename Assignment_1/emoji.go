bc935516066700cd0be3bf38f991093df7a7bd5e
Sridhar <sridhar_ramineni@epam.com>
2021-05-05 09:15:03 +0530

    Changed method name for better readability

--- Assignment_1/emoji.go
+++ Assignment_1/emoji.go
@@ -16,19 +16,19 @@ func printRandomThreeEmoji() {
     var emojiLen int = len(emoji.CodeMap())
 	var emojiPrintCount int = 0
 	rand.Seed(time.Now().UTC().UnixNano())
-	emojiKey := generateEmojiKey();
+	emojiKey := getEmojiCodeMapKeys();
 	for ; emojiPrintCount < 3; {
 		fmt.Println(emoji.CodeMap()[emojiKey[rand.Intn(emojiLen)]])	
 		emojiPrintCount++
 	}
 }
 
-func generateEmojiKey() []string {
-	var emojiesKey []string = make([]string, len(emoji.CodeMap()))
+func getEmojiCodeMapKeys() []string {
+	var emojiecodeMapKeys []string = make([]string, len(emoji.CodeMap()))
 	var i int = 0
 	for k := range emoji.CodeMap() {
-		emojiesKey[i] = k
+		emojiecodeMapKeys[i] = k
 		i++
 	}
-	return emojiesKey
+	return emojiecodeMapKeys
 }
