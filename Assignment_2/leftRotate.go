1b740bcb076088a938fcf9a2026cf838670bbe61
Sridhar <sridhar_ramineni@epam.com>
2021-05-11 10:10:20 +0530

    Added min and max conditions.

--- Assignment_2/leftRotate.go
+++ Assignment_2/leftRotate.go
@@ -4,9 +4,13 @@ import "fmt"
 
 func main() {
 	sampleSlice := []int{1, 2, 3, 4, 5, 6, 7, 8}
-	fmt.Println(sampleSlice, leftRotation(sampleSlice, 5))
+	fmt.Println(sampleSlice, leftRotate(sampleSlice, 1))
 }
 
-func leftRotation(arr []int, d int) []int {
-	return append(arr[d:cap(arr)], arr[0:d]...)
+func leftRotate(arr []int, d int) []int {
+	if d > 0 && d <= len(arr) {
+		return append(arr[d:cap(arr)], arr[0:d]...)
+	} else {
+		return arr
+	}
 }
