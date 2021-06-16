b7a3d4b5ead72e801dd1bb0d2dda4df0020671ed
Sridhar <sridhar_ramineni@epam.com>
2021-06-16 09:56:50 +0530

    Externalize DB properties.

--- Assignment_8/database/db.go
+++ Assignment_8/database/db.go
@@ -4,22 +4,31 @@ import (
 	"fmt"
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
+	"github.com/magiconair/properties"
+	"os"
+	"path/filepath"
 )
-const DB_USERNAME = "EdngiFqCZd"
-const DB_PASSWORD = "if44fVauir"
-const DB_NAME = "EdngiFqCZd"
-const DB_HOST = "remotemysql.com"
-const DB_PORT = "3306"
+
 
 var Db *gorm.DB
+
+
 func InitDb() *gorm.DB {
 	Db = connectDB()
 	return Db
 }
 
+
+
 func connectDB() (*gorm.DB) {
+	var prop = readDBPropertyFile()	
+	var db_username = prop.MustGet("db.username")
+	var db_password = prop.MustGet("db.password")
+	var db_name = prop.MustGet("db.name")
+	var db_host = prop.MustGet("db.host")
+	var db_port = prop.MustGet("db.port")
 	var err error
-	dbHostURL := DB_USERNAME +":"+ DB_PASSWORD +"@tcp"+ "(" + DB_HOST + ":" + DB_PORT +")/" + DB_NAME + "?" + "parseTime=true&loc=Local"
+	dbHostURL := db_username +":"+ db_password +"@tcp"+ "(" + db_host + ":" + db_port +")/" + db_name + "?" + "parseTime=true&loc=Local"
 	db, err := gorm.Open(mysql.Open(dbHostURL), &gorm.Config{})
 
 	if err != nil {
@@ -29,3 +38,16 @@ func connectDB() (*gorm.DB) {
 
 	return db
 }
+
+
+func readDBPropertyFile() *properties.Properties {
+	path, _ := os.Getwd()
+	var PropertyFile= []string{filepath.Join(path, "db.properties")}
+	var prop, err = properties.LoadFiles(PropertyFile, properties.UTF8, true)
+	if err != nil {
+		fmt.Println("Error in reading property file error=%v", err)
+		return nil
+	}
+	return prop
+ }
+
--- Assignment_8/go.mod
+++ Assignment_8/go.mod
@@ -14,6 +14,7 @@ require (
 	github.com/jinzhu/now v1.1.2 // indirect
 	github.com/json-iterator/go v1.1.11 // indirect
 	github.com/leodido/go-urn v1.2.1 // indirect
+	github.com/magiconair/properties v1.8.5 // indirect
 	github.com/mattn/go-isatty v0.0.13 // indirect
 	github.com/modern-go/concurrent v0.0.0-20180306012644-bacd9c7ef1dd // indirect
 	github.com/modern-go/reflect2 v1.0.1 // indirect
